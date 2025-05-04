f5389b97bd6f95d3822283d8e5fa80075710974d
jakeogh <github.com@v6y.net>
2025-05-03 19:39:31 -0700

    auto-commit

--- devicetool/__init__.py
+++ devicetool/__init__.py
@@ -1,3 +1,7 @@
+"""
+isort:skip_file
+"""
+
 # from .cli import create_filesystem
 # from .cli import destroy_block_device
 # from .cli import destroy_block_device_head_and_tail
@@ -6,12 +10,12 @@
 # from .cli import write_gpt
 # from .cli import write_grub_bios_partition
 # from .devicetool import wait_for_block_special_device_to_exist
-from .devicetool import add_partition_number_to_device
-from .devicetool import block_devices
-from .devicetool import device_is_not_a_partition
-from .devicetool import get_block_device_size
-from .devicetool import get_partuuid_for_partition
-from .devicetool import get_root_device
-from .devicetool import path_is_block_special
-from .devicetool import safety_check_devices
-from .devicetool import write_output
+from .devicetool import add_partition_number_to_device as add_partition_number_to_device
+from .devicetool import block_devices as block_devices
+from .devicetool import device_is_not_a_partition as device_is_not_a_partition
+from .devicetool import get_block_device_size as get_block_device_size
+from .devicetool import get_partuuid_for_partition as get_partuuid_for_partition
+from .devicetool import get_root_device as get_root_device
+from .devicetool import path_is_block_special as path_is_block_special
+from .devicetool import safety_check_devices as safety_check_devices
+from .devicetool import write_output as write_output
