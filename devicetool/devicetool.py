392bc4ded243d800f9994698e206aaeb5a86981e
jakeogh <github.com@v6y.net>
2025-09-25 20:11:05 -0700

    auto-commit

--- devicetool/devicetool.py
+++ devicetool/devicetool.py
@@ -1,22 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf8 -*-
 
-# pylint: disable=missing-docstring               # [C0111] docstrings are always outdated and wrong
-# pylint: disable=fixme                           # [W0511] todo is encouraged
-# pylint: disable=line-too-long                   # [C0301]
-# pylint: disable=too-many-instance-attributes    # [R0902]
-# pylint: disable=too-many-lines                  # [C0302] too many lines in module
-# pylint: disable=invalid-name                    # [C0103] single letter var names, name too descriptive
-# pylint: disable=too-many-return-statements      # [R0911]
-# pylint: disable=too-many-branches               # [R0912]
-# pylint: disable=too-many-statements             # [R0915]
-# pylint: disable=too-many-arguments              # [R0913]
-# pylint: disable=too-many-nested-blocks          # [R1702]
-# pylint: disable=too-many-locals                 # [R0914]
-# pylint: disable=too-few-public-methods          # [R0903]
-# pylint: disable=no-member                       # [E1101] no member for base
-# pylint: disable=attribute-defined-outside-init  # [W0201]
-# pylint: disable=too-many-boolean-expressions    # [R0916] in if statement
 from __future__ import annotations
 
 import os
@@ -153,30 +137,6 @@ def device_is_not_a_partition(
     if device.name.startswith("nvme") or device.name.startswith("mmcblk"):
         assert device.name[-2] != "p"
     return True
-
-
-# pathtool
-# def wait_for_block_special_device_to_exist(
-#    *,
-#    device: Path,
-#    timeout: int = 5,
-# ):
-#    device = Path(device)
-#    eprint(f"waiting for block special device: {device.as_posix()} to exist")
-#    start = time.time()
-#    if path_exists(device):
-#        assert path_is_block_special(device, symlink_ok=True)
-#        return True
-#
-#    while not path_exists(device):
-#        time.sleep(0.1)
-#        if time.time() - start > timeout:
-#            raise TimeoutError(
-#                f"timeout waiting for block special device: {device} to exist"
-#            )
-#        if path_is_block_special(device, symlink_ok=True):
-#            break
-#    return True
 
 
 def add_partition_number_to_device(
